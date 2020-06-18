239e478a99c8d383629d26b8e863001606723385
YuanGenghis <wsyztz@gmail.com>
2020-06-17 22:27:12 -0400

    fix

--- src/components/SearchRowComponent.js
+++ src/components/SearchRowComponent.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import AmazonService from "../services/AmazonService";
 
 export default class SearchRowComponent extends React.Component {
     state = {
